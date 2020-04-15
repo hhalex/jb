c84a3e1c00577bdfa30dd1a2008fb8904e833c26
Alexandre Careil <alex.careil.fr@gmail.com>
2020-04-15 19:24:17 +0200

    Add a first route to push a codebase

--- build.sbt
+++ build.sbt
@@ -1,18 +1,19 @@
 
 scalaVersion := "2.13.1"
 
-libraryDependencies += "co.fs2" %% "fs2-core" % "2.0.1"
-libraryDependencies += "co.fs2" %% "fs2-io" % "2.0.1"
+libraryDependencies += "co.fs2" %% "fs2-core" % "2.3.0"
+libraryDependencies += "co.fs2" %% "fs2-io" % "2.3.0"
 
-libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
-libraryDependencies += "org.typelevel" %% "cats-effect" % "2.0.0"
+libraryDependencies += "org.typelevel" %% "cats-core" % "2.1.1"
+libraryDependencies += "org.typelevel" %% "cats-kernel" % "2.1.1"
+libraryDependencies += "org.typelevel" %% "cats-effect" % "2.1.1"
 
-libraryDependencies += "org.http4s" %% "http4s-core" % "0.21.0-M5"
-libraryDependencies += "org.http4s" %% "http4s-blaze-client" % "0.21.0-M5"
-libraryDependencies += "org.http4s" %% "http4s-blaze-server" % "0.21.0-M5"
-libraryDependencies += "org.http4s" %% "http4s-dsl" % "0.21.0-M5"
-libraryDependencies += "org.http4s" %% "http4s-dsl" % "0.21.0-M5" % Test
+libraryDependencies += "org.http4s" %% "http4s-core" % "0.21.3"
+libraryDependencies += "org.http4s" %% "http4s-blaze-client" % "0.21.3"
+libraryDependencies += "org.http4s" %% "http4s-blaze-server" % "0.21.3"
+libraryDependencies += "org.http4s" %% "http4s-dsl" % "0.21.3"
+libraryDependencies += "org.http4s" %% "http4s-dsl" % "0.21.3" % Test
 
-libraryDependencies += "org.http4s" %% "http4s-circe" % "0.21.0-M5"
+libraryDependencies += "org.http4s" %% "http4s-circe" % "0.21.3"
 
-libraryDependencies += "io.circe" %% "circe-generic" % "0.12.3"
+libraryDependencies += "io.circe" %% "circe-generic" % "0.13.0"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,108 @@
-package exystence
+package com.hhalex.jb
 
+import java.nio.file.Paths
 
-import cats.effect.{ExitCode, IO, IOApp}
-import cats.implicits._
+import cats.effect.{Blocker, ExitCode, IO, IOApp}
 import org.http4s._
 import org.http4s.dsl.io._
 import org.http4s.implicits._
+import org.http4s.multipart.{Multipart}
 import org.http4s.server.blaze.BlazeServerBuilder
+import fs2.{io,_}
+
+import sys.process._
+import cats.data._
+import cats.data.Validated._
+import cats.implicits._
+
+trait Read[A] {
+  def read(s: String): Option[A]
+}
+
+object Read {
+  def apply[A](implicit A: Read[A]): Read[A] = A
+
+  implicit val stringRead: Read[String] =
+    new Read[String] { def read(s: String): Option[String] = Some(s) }
+
+  implicit val intRead: Read[Int] =
+    new Read[Int] {
+      def read(s: String): Option[Int] =
+        if (s.matches("-?[0-9]+")) Some(s.toInt)
+        else None
+    }
+}
+
+object PushCodebaseValidator {
+  sealed trait PushCodebaseRequestValidation
+  case class ParseError(str: String) extends PushCodebaseRequestValidation
+  case class MissingField(field: String) extends PushCodebaseRequestValidation
+  case class MissingFile(fileName: String) extends PushCodebaseRequestValidation
+  type ValidationResult[A] = Validated[PushCodebaseRequestValidation, A]
+
+  private def extractField[T: Read](m: Multipart[IO], fieldName: String): ValidationResult[T] = {
+    val optValue = for {
+      part <- m.parts.find(_.name.contains(fieldName))
+      value <- part.body.through(fs2.text.utf8Decode).compile.toList.unsafeRunSync().headOption
+    } yield value
+
+    optValue match {
+      case Some(strValue) => Read[T].read(strValue) match {
+        case Some(v) => Valid(v)
+        case None => Invalid(ParseError(strValue))
+      }
+      case None => Invalid(MissingField(fieldName))
+    }
+  }
+
+  private def extractFile(m: Multipart[IO], fileName: String): ValidationResult[Stream[IO, Byte]] =
+    m.parts.find(_.filename.contains(fileName)).map(_.body) match {
+      case Some(value) => Valid(value)
+      case None => Invalid(MissingFile(fileName))
+    }
+
+  def validateRequest(m: Multipart[IO]): ValidatedNec[PushCodebaseRequestValidation, PushCodebaseRequest] =
+    (
+      extractFile(m, "codebase.tar.gz").toValidatedNec,
+      extractField[String](m, "app").toValidatedNec,
+      extractField[String](m, "rev").toValidatedNec,
+      extractField[Int](m, "conf").toValidatedNec,
+      extractField[Int](m, "target").toValidatedNec
+    ).mapN(PushCodebaseRequest)
+}
+case class PushCodebaseRequest(codebase: fs2.Stream[IO, Byte], app: String, revision: String, config: Int, target: Int)
+case class BundleRequest(app: String, version: String)
 
 object Main extends IOApp {
   def run(args: List[String]) = {
 
+    val blocker = Blocker[IO]
+
     val app = HttpRoutes
       .of[IO] {
-        case Method.GET -> Root  => Ok("coucou")
+        case req@(Method.GET -> Root) => {
+          Ok(req.body+"2")
+        }
+        case request@(Method.POST -> Root / "codebase") => {
+          blocker.use {b => {
+            request.decode[Multipart[IO]] { m => {
+              PushCodebaseValidator.validateRequest(m) match {
+                case Valid(a) => {
+                  val writeCodebase: IO[Unit] = a.codebase.through(io.file.writeAll(Paths.get("codebase.tar.gz"), b)).compile.drain
+
+                  Ok(for {
+                    _ <- writeCodebase
+                    _ <- fs2.io.file.createDirectories[IO](b, Paths.get(a.app, a.revision, a.config.toString, a.target.toString))
+                    _ <- IO.pure(s"tar -xvzf codebase.tar.gz --directory ${a.app}/${a.revision}/${a.config}/${a.target}".!)
+                    res <- IO.pure(s"ls -al ${a.app}/${a.revision}/${a.config}/${a.target}".!!)
+                  } yield res)
+                }
+                case Invalid(errorList) => Ok(errorList.foldMap(_.toString))
+              }
+            }}
+          }}
+
+        }
       }
       .orNotFound
 
